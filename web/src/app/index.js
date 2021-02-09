08032f5fec6bbf26ed7f37d4c6c5ad722299add2
massyao <massyao@126.com>
2021-02-09 16:28:21 +0800

    update index page

--- web/src/app/index.js
+++ web/src/app/index.js
@@ -4,8 +4,6 @@ import { Helmet } from 'react-helmet';
 import Modals from '@app/modals';
 
 import Main from '@app/main';
-import About from '@app/about';
-import NotFound from '@app/not-found';
 
 import 'antd/dist/antd.css';
 import '@theme/style.less';
@@ -14,12 +12,10 @@ function App() {
   return (
     <Fragment>
       <Helmet>
-        <title>PeerJS & React & Redux</title>
+        <title>Real Time Chat</title>
       </Helmet>
       <Switch>
         <Route path="/" exact={true} component={Main} />
-        <Route path="/about" exact={true} component={About} />
-        <Route component={NotFound} />
       </Switch>
       <Modals />
     </Fragment>
